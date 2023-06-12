ef8e266c61d871d938ec5cc3f647c2b49fa20416
bumwhat <106128499+bumwhat@users.noreply.github.com>
2023-06-11 21:16:04 -0500

    Create reddream.lua

---
+++ reddream.lua
@@ -0,0 +1,55 @@
+local args = {
+    [1] = "m4a4-beyond",
+    [2] = "Factory New"
+}
+
+game:GetService("ReplicatedStorage").Remotes.SellSkin:FireServer(unpack(args))
+
+local args = {
+    [1] = "m4a4-beyond",
+    [2] = "Factory New"
+}
+
+game:GetService("ReplicatedStorage").Remotes.SellSkin:FireServer(unpack(args))
+
+local args = {
+    [1] = "m4a1s-arcadia",
+    [2] = "Factory New"
+}
+
+game:GetService("ReplicatedStorage").Remotes.SellSkin:FireServer(unpack(args))
+
+local args = {
+    [1] = "m4a1s-arcadia",
+    [2] = "Factory New"
+}
+
+game:GetService("ReplicatedStorage").Remotes.SellSkin:FireServer(unpack(args))
+
+local args = {
+    [1] = "ak47-harmony",
+    [2] = "Factory New"
+}
+
+game:GetService("ReplicatedStorage").Remotes.SellSkin:FireServer(unpack(args))
+
+local args = {
+    [1] = "ak47-harmony",
+    [2] = "Factory New"
+}
+
+game:GetService("ReplicatedStorage").Remotes.SellSkin:FireServer(unpack(args))
+
+local args = {
+    [1] = "awp-immortal",
+    [2] = "Factory New"
+}
+
+game:GetService("ReplicatedStorage").Remotes.SellSkin:FireServer(unpack(args))
+
+local args = {
+    [1] = "awp-immortal",
+    [2] = "Factory New"
+}
+
+game:GetService("ReplicatedStorage").Remotes.SellSkin:FireServer(unpack(args))
