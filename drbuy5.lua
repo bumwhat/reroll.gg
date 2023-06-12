47d89804c9e84ca2e857ff3171385dff52760bfb
bumwhat <106128499+bumwhat@users.noreply.github.com>
2023-06-11 21:20:47 -0500

    Update drbuy5.lua

--- drbuy5.lua
+++ drbuy5.lua
@@ -1,38 +1,19 @@
-local drbuy5
+local args = {
+    [1] = "dream_case",
+    [2] = 5
+}
 
-drbuy:NewToggle("Buy/Open x5", "Buy and Open Dream Cases", function(state)
-    if state then
-        if not drbuy5 then
-            print("Toggle On")
-            drbuy5 = true
-            while drbuy5 do
-                local args = {
-                    [1] = "dream_case",
-                    [2] = 5
-                }
-                
-                game:GetService("ReplicatedStorage").Remotes.BuyBulk:FireServer(unpack(args))
-                
-                local args = {
-                    [1] = "dream_case",
-                    [2] = 5
-                }
-                
-                game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-                
-                local args = {
-                    [1] = 8455102
-                }
-                
-                game:GetService("ReplicatedStorage").Remotes.GetPassStatus:InvokeServer(unpack(args))
-                
-                wait(5.5)
-            end
-        end
-    else
-        if drbuy5 then
-            print("Toggled Off")
-            drbuy5 = false
-        end
-    end
-end)
+game:GetService("ReplicatedStorage").Remotes.BuyBulk:FireServer(unpack(args))
+
+local args = {
+    [1] = "dream_case",
+    [2] = 5
+}
+
+game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+
+local args = {
+    [1] = 8455102
+}
+
+game:GetService("ReplicatedStorage").Remotes.GetPassStatus:InvokeServer(unpack(args))
